84e3716d916c6c41397fc5b329baf7b358ab2f44
VionaDankil <manja.h@gmail.com>
2026-05-31 13:10:11 +0200

    Add files via upload

---
+++ gemini-code-1780225792243.sh
@@ -0,0 +1,6 @@
+git init
+git add streamlit_app.py requirements.txt
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/DEIN_BENUTZERNAME/DEIN_REPOSITORY_NAME.git
+git push -u origin main
